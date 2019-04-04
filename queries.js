a4119725158ea1789c2bfe799c2bdf46536d88cf
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-03 22:14:09 -0400

    Minor typo fix

--- queries.js
+++ queries.js
@@ -34,7 +34,7 @@ const createUser = (request, response) => {
     if (error) {
       throw error
     }
-    response.status(201).send(`User added with ID: ${result.insertId}`)
+    response.status(201).send(`User added with ID: ${results.insertId}`)
   })
 }
 
